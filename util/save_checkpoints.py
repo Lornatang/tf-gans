fca24f8557aeed5b64a354a9c3abef4b7356ab99
Lornatang <shiyipaisizuo@163.com>
2019-05-23 00:38:07 +0800

    Save the model in training to the specified path

---
+++ util/save_checkpoints.py
@@ -0,0 +1,33 @@
+# Copyright 2019 ChangyuLiu Authors. All Rights Reserved.
+#
+#           Licensed under the MIT License.
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+#         https://opensource.org/licenses/MIT
+# ==============================================================================
+
+"""save model function"""
+
+import os
+import tensorflow as tf
+
+
+def save_checkpoints(generator, discriminator, generator_optimizer, discriminator_optimizer, save_path):
+  """ save gan model
+
+  Args:
+    generator: generate model.
+    discriminator: discriminate model.
+    generator_optimizer: generate optimizer func.
+    discriminator_optimizer: discriminator optimizer func.
+    save_path: save gan model dir path.
+
+  Returns:
+    none
+
+  """
+  os.path.join(save_path, 'ckpt')
+  tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                      discriminator_optimizer=discriminator_optimizer,
+                      generator=generator,
+                      discriminator=discriminator)
