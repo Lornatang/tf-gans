2f60c72629b799ca9a980942e9c373f5731b7904
Lornatang <shiyipaisizuo@163.com>
2019-05-23 00:30:35 +0800

    Save the pictures after training to the specified folder

---
+++ util/generate_and_save_images.py
@@ -0,0 +1,39 @@
+# Copyright 2019 ChangyuLiu Authors. All Rights Reserved.
+#
+#           Licensed under the MIT License.
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+#         https://opensource.org/licenses/MIT
+# ==============================================================================
+
+"""implements basic function"""
+
+from matplotlib import pyplot as plt
+
+
+def generate_and_save_images(model, epoch, seed, save_path):
+  """make sure the training parameter is set to False because we
+     don't want to train the batchnorm layer when doing inference.
+
+  Args:
+    model: generate model.
+    epoch: train epoch nums.
+    seed: random seed at (16, 100).
+    save_path: generate images path.
+
+  Returns:
+    none.
+
+  """
+  predictions = model(seed, trainning=False)
+
+  fig = plt.figure(figsize=(4, 4))
+
+  for i in range(predictions.shape[0]):
+    plt.subplot(4, 4, i+1)
+    plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gary')
+    plt.axis('off')
+
+  plt.savefig(save_path + f'{epoch:04d}.png')
+  plt.show()
+  plt.close(fig)
