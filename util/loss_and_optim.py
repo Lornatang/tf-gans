3732461826ecad222acd365369991dda4686304e
Lornatang <shiyipaisizuo@163.com>
2019-05-23 16:31:51 +0800

    Fixed a fatal error

--- util/loss_and_optim.py
+++ util/loss_and_optim.py
@@ -43,7 +43,7 @@ def discriminator_loss(real_output, fake_output):
 
   """
   real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-  fake_loss = cross_entropy(tf.ones_like(fake_output), fake_output)
+  fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
   total_loss = real_loss + fake_loss
 
   return total_loss
