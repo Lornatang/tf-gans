29e60e48404a30573fbc7ec66e519430b2b1de55
Lornatang <shiyipaisizuo@163.com>
2019-05-23 13:39:55 +0800

    The model is transformed into GAN network architecture

--- network/discriminator.py
+++ network/discriminator.py
@@ -22,16 +22,14 @@ def make_discriminator_model():
 
   """
   model = tf.keras.models.Sequential()
-  model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                          input_shape=[28, 28, 1]))
-  model.add(layers.LeakyReLU())
-  model.add(layers.Dropout(0.3))
-
-  model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-  model.add(layers.LeakyReLU())
-  model.add(layers.Dropout(0.3))
-
   model.add(layers.Flatten())
-  model.add(layers.Dense(1))
+
+  model.add(layers.Dense(512))
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Dense(256))
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Dense(1, activation=tf.nn.sigmoid))
 
   return model
--- network/generator.py
+++ network/generator.py
@@ -11,8 +11,6 @@
 import tensorflow as tf
 from tensorflow.python.keras import layers
 
-DIM = 100
-
 
 def make_generator_model():
   """ implements generate.
@@ -22,24 +20,18 @@ def make_generator_model():
 
   """
   model = tf.keras.models.Sequential()
-  model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
+  model.add(layers.Dense(256, input_dim=100))
+  model.add(layers.LeakyReLU())
+
+  model.add(layers.Dense(512))
   model.add(layers.BatchNormalization())
   model.add(layers.LeakyReLU())
 
-  model.add(layers.Reshape((7, 7, 256)))
-  assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
-
-  model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-  assert model.output_shape == (None, 7, 7, 128)
+  model.add(layers.Dense(1024))
   model.add(layers.BatchNormalization())
   model.add(layers.LeakyReLU())
 
-  model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-  assert model.output_shape == (None, 14, 14, 64)
-  model.add(layers.BatchNormalization())
-  model.add(layers.LeakyReLU())
-
-  model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-  assert model.output_shape == (None, 28, 28, 1)
+  model.add(layers.Dense(28 * 28 * 1, activation=tf.nn.tanh))
+  model.add(layers.Reshape((28, 28, 1)))
 
   return model
